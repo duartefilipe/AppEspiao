90264c3524dde01eb2f0ee51d6ba98f19870f419
duartefilipe <filipeduarte.si@gmail.com>
2016-11-22 00:04:20 -0200

    Update PrincipalActivity.java
    pegando localização comentado

--- app/src/main/java/com/example/anakin/appespiao2/PrincipalActivity.java
+++ app/src/main/java/com/example/anakin/appespiao2/PrincipalActivity.java
@@ -1,17 +1,48 @@
 package com.example.anakin.appespiao2;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
-
-/**
- * Created by anakin on 15/11/16.
- */
+import android.util.Log;
+import android.widget.TextView;
 
 public class PrincipalActivity extends AppCompatActivity {
+
+    private TextView txtLatitude;
+    private TextView txtLongitude;
+
+
+    private Location location;
+    private LocationManager locationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+/*
+        txtLatitude = (TextView) findViewById(R.id.txtLatitude);
+        txtLongitude = (TextView) findViewById(R.id.TxtLongitude);
+
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+
+        }else{
+            locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+            location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        }
+
+        double longitude = location.getLongitude();
+        double latitude = location.getLatitude();
+
+        txtLongitude.setText("Longitude: " + longitude);
+        txtLatitude.setText("Latitude: " + latitude);
+*/
     }
+
+
 }
