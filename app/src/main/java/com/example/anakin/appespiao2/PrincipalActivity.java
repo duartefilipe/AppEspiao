caaa24097bf165bd474b2a07b90903dc5738ca9e
duartefilipe <filipeduarte.si@gmail.com>
2016-11-24 01:28:17 -0200

    Update PrincipalActivity.java
    funcionando essa merda, mostrando no print e na view, latitude e longitude

--- app/src/main/java/com/example/anakin/appespiao2/PrincipalActivity.java
+++ app/src/main/java/com/example/anakin/appespiao2/PrincipalActivity.java
@@ -3,6 +3,7 @@ package com.example.anakin.appespiao2;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
@@ -11,38 +12,49 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.TextView;
 
+/**
+ * Created by anakin on 15/11/16.
+ */
+
 public class PrincipalActivity extends AppCompatActivity {
 
     private TextView txtLatitude;
     private TextView txtLongitude;
+    public Location location;
+    public LocationManager locationManager;
 
-
-    private Location location;
-    private LocationManager locationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-/*
+
+
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
         txtLatitude = (TextView) findViewById(R.id.txtLatitude);
-        txtLongitude = (TextView) findViewById(R.id.TxtLongitude);
-
-
+        txtLongitude = (TextView) findViewById(R.id.txtLongitude);
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
-
+            System.out.println("-----------------------------aqui no if");
         }else{
+            System.out.println("-----------------------------aqui no else");
             locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
             location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
         }
+
+        System.out.println("-----------------------------depois do else");
+
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
 
         double longitude = location.getLongitude();
         double latitude = location.getLatitude();
 
         txtLongitude.setText("Longitude: " + longitude);
         txtLatitude.setText("Latitude: " + latitude);
-*/
+
+        System.out.println("-----------------------------Latitude" +latitude);
+        System.out.println("-----------------------------Longitude" +longitude);
+
+        
     }
-
-
 }
